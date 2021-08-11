85b0991464264aaf94c16a8e6d2e292bb46f411d
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-08-11 16:07:42 -0600

    updated from lost content

--- public/quiz.js
+++ public/quiz.js
@@ -3,6 +3,7 @@ const form = document.querySelector("form");
 const tryAgain = document.getElementById("try-again");
 const checkAns = document.getElementById("check-answers");
 const quesSection = document.querySelector(".question-section");
+const showAns = document.querySelectorAll(".show-score");
 const baseURL = "https://opentdb.com/api.php";
 
 const getInput = (e) => {
@@ -45,7 +46,6 @@ const getInput = (e) => {
 };
 
 getQuiz = (gameChoice) => {
-  console.log("in quiz");
   axios
     .get(`${baseURL}?${gameChoice}`)
     .then((res) => {
@@ -54,12 +54,7 @@ getQuiz = (gameChoice) => {
       checkAns.style.display = "block";
       quesSection.style.display = "block";
 
-      let quizQuestions = "";
-      let singleQues = "";
-      let answerOptions = [];
-      let correct = 0;
-      quesSection.innerHTML = "";
-      let ansOptionsSection = "";
+      let numCorrect = 0;
 
       quizQuestions = res.data.results;
       console.log(quizQuestions);
@@ -69,107 +64,85 @@ getQuiz = (gameChoice) => {
         singleQues.className = `Question${i + 1}`;
         singleQues.innerHTML = `<h3>${quizQuestions[i].question}</h3>`;
         quesSection.appendChild(singleQues);
+        let correctAns = quizQuestions[i].correct_answer;
 
         answerOptions = [...quizQuestions[i].incorrect_answers];
 
-        //ensure answer is placed randomly into answers
-        answerOptions.splice(
-          Math.floor(Math.random() * 3),
-          0,
-          quizQuestions[i].correct_answer
-        );
-        console.log(answerOptions);
+        answerOptions.splice(Math.floor(Math.random() * 3), 0, correctAns);
 
         ansOptionsSection = document.createElement("div");
 
         for (let j = 0; j < answerOptions.length; j++) {
-          ansOptionsSection.innerHTML += `<div class='answer-container' id="option${
+          const ansHTML = `<div class='answer-container' id="option${
             j + 1
           }-${i}-container"><input type="radio" class="Question${i}" id="option${
             j + 1
           }-${i}" name="answer${i}" value="${answerOptions[j]}">
           <label for="option${j + 1}-${i}">${answerOptions[j]}</label></div>`;
+          ansOptionsSection.innerHTML += ansHTML;
           singleQues.appendChild(ansOptionsSection);
         }
       }
       checkAnswers = () => {
         checkAns.style.display = "none";
         for (let m = 0; m < quizQuestions.length; m++) {
-          // console.log(`answer${m}`);
           const items = document.getElementsByName(`answer${m}`);
+          const question = document.querySelector(`.Question${m + 1}`);
+          let correct = quizQuestions[m].correct_answer;
+          const inputSpot = document.querySelector(
+            `input[value = "${correct}"][class="Question${m}"]`
+          );
+
+          console.log(quizQuestions[m].question);
+          console.log(`correct ans: ${correct}`);
 
           for (const item of items) {
             if (item.checked) {
-              // console.log(typeof item.value);
-              // console.log(typeof quizQuestions[m].correct_answer);
-              document.querySelector(
-                `.Question${m + 1}`
-              ).style.backgroundColor = "";
-              if (item.value === quizQuestions[m].correct_answer) {
-                // console.log("correct answer found");
-                document.querySelector(
-                  `.Question${m + 1}`
-                ).style.backgroundColor = "rgba(82, 243, 61, 0.479)";
-                correct++;
+              if (item.value === correct) {
+                console.log("correct answer found");
+                question.style.backgroundColor = "rgba(82, 243, 61, 0.479)";
+                numCorrect++;
               } else {
                 console.log("wrong answer found");
-                console.log(quizQuestions[m].question);
-
-                console.log(quizQuestions[m].correct_answer);
-                if (
-                  document.querySelector(
-                    `input[value = "${quizQuestions[m].correct_answer}"]`
-                  ) !== null
-                ) {
-                  console.log("not null");
-                  let inputSpot = "";
-                  inputSpot = document.querySelector(
-                    `input[value = "${quizQuestions[m].correct_answer}"]`
-                  ).id;
-                  console.log(`${inputSpot}-container`);
-                  console.log(
-                    document.getElementById(`${inputSpot}-container`)
+                if (inputSpot !== null) {
+                  let inputSpotId = inputSpot.id;
+                  const correctContainer = document.getElementById(
+                    `${inputSpotId}-container`
                   );
-                  console.log(inputSpot);
-                  document.getElementById(
-                    `${inputSpot}-container`
-                  ).style.backgroundColor = "yellow";
+                  correctContainer.style.backgroundColor = "yellow";
                 }
-
-                document.querySelector(
-                  `.Question${m + 1}`
-                ).style.backgroundColor = "rgba(212, 3, 3, 0.425)";
+                question.style.backgroundColor = "rgba(212, 3, 3, 0.425)";
               }
               console.log("---------");
             }
           }
         }
-        let showAns = document.querySelectorAll(".show-score");
         for (let spot in showAns) {
           showAns[
             spot
-          ].innerHTML = `You got <span>${correct}</span> out of <span>${quizQuestions.length}</span> correct!`;
+          ].innerHTML = `You got <span>${numCorrect}</span> out of <span>${quizQuestions.length}</span> correct!`;
         }
       };
-
       checkAns.addEventListener("click", checkAnswers);
     })
     .catch((error) => console.log(error));
 }; //end of getQuiz function
 
 reset = () => {
-  let showAns = document.querySelectorAll(".show-score");
-  for (let spot in showAns) {
-    showAns[spot].innerHTML = "";
-  }
-  form.style.display = "block";
-  quesSection.style.display = "none";
-  tryAgain.style.display = "none";
-  checkAns.style.display = "none";
+  // let showAns = document.querySelectorAll(".show-score");
+  // for (let spot in showAns) {
+  //   showAns[spot].innerHTML = "";
+  // }
+  // form.style.display = "block";
+  // quesSection.style.display = "none";
+  // tryAgain.style.display = "none";
+  // checkAns.style.display = "none";
   // let ansContainers = document.getElementsByClassName("answer-containers");
   // for (let container in ansContainers) {
   //   container.style.backgroundColor = "";
   // }
+
+  location.reload();
 };
 
 generate.addEventListener("click", getInput);
